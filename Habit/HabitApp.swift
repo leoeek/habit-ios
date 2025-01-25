fc66350c10623cca10b4c6ac423bd79eaa1cd9a1
leoprocopio <leonardolp@gmail.com>
2025-01-25 17:49:07 -0300

    Initial Commit

---
+++ Habit/HabitApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitApp.swift
+//  Habit
+//
+//  Created by Leonardo Lima Procópio on 25/01/25.
+//
+
+import SwiftUI
+
+@main
+struct HabitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HabitTests/HabitTests.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTests.swift
+//  HabitTests
+//
+//  Created by Leonardo Lima Procópio on 25/01/25.
+//
+
+import Testing
+@testable import Habit
+
+struct HabitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
