32b59867c6970fe4fd7ee0b0ff56bdba98119088
leoprocopio <leonardolp@gmail.com>
2025-03-23 10:47:02 -0300

    Add combine to login.

---
+++ Habit/Common/Network/RemoteDataSource.swift
@@ -0,0 +1,16 @@
+//
+//  RemotoDataSource.swift
+//  Habit
+//
+//  Created by Leonardo Lima Procópio on 23/03/25.
+//
+
+import Foundation
+
+class RemotoDataSource {
+    //padrao singleton
+    
+    private init() {
+        
+    }
+}
